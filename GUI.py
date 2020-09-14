3441396a9198238afca5535111b84a390eed7084
AlekseyVolok <57563824+AlekseyVolok@users.noreply.github.com>
2020-09-14 17:08:14 +0200

    updated

--- GUI.py
+++ GUI.py
@@ -1,123 +1,125 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
-import book
+from book import Database
+
+database = Database()
+class Window(object):
+    def __init__(self,window):
+        self.window = window
+        self.window.wm_title("Book Reminder")
+        l1 = Label(window,text="Title")
+        l1.grid(row=0,column=0)
+
+        l2 = Label(window,text="Author")
+        l2.grid(row=0,column=2)
+
+        l3 = Label(window,text="Rating")
+        l3.grid(row=1,column=0)
+
+        l4 = Label(window,text="Finished")
+        l4.grid(row=1,column=2)
+
+        l5 = Label(window,text="Opinion ")
+        l5.grid(row=0,column=4)
 
 
-def get_selected_row(event):
-    try:
-        global selected_tuple
-        index=list1.curselection()[0]
-        selected_tuple=list1.get(index)
-        e1.delete(0,END)
-        e1.insert(END,selected_tuple[1])
-        e2.delete(0,END)
-        e2.insert(END,selected_tuple[2])
-        e3.delete(0,END)
-        e3.insert(END,selected_tuple[3])
-        e4.delete(0,END)
-        e4.insert(END,selected_tuple[4])
-        e5.delete('1.0','end-1c')
-        e5.insert(END,selected_tuple[5])
-    except IndexError:
-        pass
+        self.title_text = StringVar()
+        self.e1 = Entry(window,textvariable=self.title_text)
+        self.e1.grid(row=0,column=1)
+
+        self.author_text = StringVar()
+        self.e2 = Entry(window,textvariable=self.author_text)
+        self.e2.grid(row=0,column=3)
+
+        self.rating_text = StringVar()
+        self.e3 = Entry(window,textvariable=self.rating_text)
+        self.e3.grid(row=1,column=1)
+
+        self.finished_text = StringVar()
+        self.e4 = Entry(window,textvariable=self.finished_text)
+        self.e4.grid(row=1,column=3)
+
+
+
+        self.e5 = ScrolledText(window,height=6,width=35)
+        self.e5.grid(row=1,column=5)
+
+        self.list1 = Listbox(window,height=10,width=60)
+        self.list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+        sb1 = Scrollbar(window)
+        sb1.grid(row=2,column=2,rowspan=6)
+
+        self.list1.configure(yscrollcommand=sb1.set)
+        sb1.configure(command=self.list1.yview)
+
+
+        self.list1.bind('<<ListboxSelect>>',self.get_selected_row) #bind selected row with event
+
+        b1 = Button(window,text="View all",width=12,command=self.view_command)
+        b1.grid(row=2,column=3)
+
+        b2 = Button(window,text="Add entry",width=12,command=self.add_command)
+        b2.grid(row=3,column=3)
+
+        b3 = Button(window,text="Search entry",width=12,command=self.search_command)
+        b3.grid(row=4,column=3)
+
+        b4 = Button(window,text="Update selected",width=12,command=self.update_command)
+        b4.grid(row=5,column=3)
+
+        b5 = Button(window,text="Delete selected",width=12,command=self.delete_command)
+        b5.grid(row=6,column=3)
+
+        b6 = Button(window,text="Close",width=12,command=window.destroy)
+        b6.grid(row=7,column=3)
+
+
+
+
+    def get_selected_row(self,event):
+        try:
+            index = self.list1.curselection()[0]
+            self.selected_tuple = self.list1.get(index)
+            self.e1.delete(0,END)
+            self.e1.insert(END,self.selected_tuple[1])
+            self.e2.delete(0,END)
+            self.e2.insert(END,self.selected_tuple[2])
+            self.e3.delete(0,END)
+            self.e3.insert(END,self.selected_tuple[3])
+            self.e4.delete(0,END)
+            self.e4.insert(END,self.selected_tuple[4])
+            self.e5.delete('1.0','end-1c')
+            self.e5.insert(END,self.selected_tuple[5])
+        except IndexError:
+            pass
     
     
 
+    def view_command(self):
+        self.list1.delete(0,END)
+        for row in database.view():
+            self.list1.insert(END,row)
 
-def view_command():
-    list1.delete(0,END)
-    for row in book.view():
-        list1.insert(END,row)
 
+    def search_command(self):
+        self.list1.delete(0,END)
+        for row in database.search(self.title_text.get(),self.author_text.get()):
+            self.list1.insert(END,row)
 
-def search_command():
-    list1.delete(0,END)
-    for row in book.search(title_text.get(),author_text.get()):
-        list1.insert(END,row)
+    def add_command(self):
+        database.add_book(self.title_text.get(),self.author_text.get(),self.rating_text.get(),self.finished_text.get(),self.e5.get('1.0','end-1c'))
+        self.list1.delete(0,END)
+        self.list1.insert(END,(self.title_text.get(),self.author_text.get(),self.rating_text.get(),self.finished_text.get(),self.e5.get('1.0','end-1c')))
 
-def add_command():
-    book.add_book(title_text.get(),author_text.get(),rating_text.get(),finished_text.get(),e5.get('1.0','end-1c'))
-    list1.delete(0,END)
-    list1.insert(END,(title_text.get(),author_text.get(),rating_text.get(),finished_text.get(),e5.get('1.0','end-1c')))
+    def delete_command(self):
+        database.delete(self.selected_tuple[0])  
 
-def delete_command():
-    book.delete(selected_tuple[0])  
-
-def update_command():
-   book.update(selected_tuple[0],title_text.get(),author_text.get(),rating_text.get(),finished_text.get(),e5.get('1.0','end-1c')) 
+    def update_command(self):
+        database.update(self.selected_tuple[0],self.title_text.get(),self.author_text.get(),self.rating_text.get(),self.finished_text.get(),self.e5.get('1.0','end-1c')) 
 
 window = Tk()
-
-window.wm_title("Book Reminder")
-
-l1 = Label(window,text="Title")
-l1.grid(row=0,column=0)
-
-l2 = Label(window,text="Author")
-l2.grid(row=0,column=2)
-
-l3 = Label(window,text="Rating")
-l3.grid(row=1,column=0)
-
-l4 = Label(window,text="Finished")
-l4.grid(row=1,column=2)
-
-l5 = Label(window,text="Opinion ")
-l5.grid(row=0,column=4)
-
-
-title_text = StringVar()
-e1 = Entry(window,textvariable=title_text)
-e1.grid(row=0,column=1)
-
-author_text = StringVar()
-e2 = Entry(window,textvariable=author_text)
-e2.grid(row=0,column=3)
-
-rating_text = StringVar()
-e3 = Entry(window,textvariable=rating_text)
-e3.grid(row=1,column=1)
-
-finished_text = StringVar()
-e4 = Entry(window,textvariable=finished_text)
-e4.grid(row=1,column=3)
-
-
-
-e5 = ScrolledText(window,height=6,width=35)
-e5.grid(row=1,column=5)
-
-list1 = Listbox(window,height=10,width=60)
-list1.grid(row=2,column=0,rowspan=6,columnspan=2)
-
-sb1 = Scrollbar(window)
-sb1.grid(row=2,column=2,rowspan=6)
-
-list1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=list1.yview)
-
-
-list1.bind('<<ListboxSelect>>',get_selected_row) #bind selected row with event
-
-b1 = Button(window,text="View all",width=12,command=view_command)
-b1.grid(row=2,column=3)
-
-b2 = Button(window,text="Add entry",width=12,command=add_command)
-b2.grid(row=3,column=3)
-
-b3 = Button(window,text="Search entry",width=12,command=search_command)
-b3.grid(row=4,column=3)
-
-b4 = Button(window,text="Update selected",width=12,command=update_command)
-b4.grid(row=5,column=3)
-
-b5 = Button(window,text="Delete selected",width=12,command=delete_command)
-b5.grid(row=6,column=3)
-
-b6 = Button(window,text="Close",width=12,command=window.destroy)
-b6.grid(row=7,column=3)
-
-
+Window(window)
 window.mainloop()
 
 
--- book.py
+++ book.py
@@ -1,65 +1,41 @@
 import psycopg2
 
 
-# try:
-#     conn = psycopg2.connect("dbname='books' user='postgres' password='admin' host='localhost' port='5432'")
-#     print("Data base connected successfully")
-# except(Exception, psycopg2.Error) as error:
-#     print("Error while connecting to PostgreSQL", error)
+class Database:
+
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            "dbname='books' user='postgres' password='admin' host='localhost' port='5432'")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS mybooks(id SERIAL PRIMARY KEY NOT NULL,title VARCHAR(100) NOT NULL, author VARCHAR(100) NOT NULL,rating FLOAT,finished DATE,opinion TEXT)")
+        self.conn.commit()
+        print("Data base connected successfully")
 
 
+    def add_book(self,title, author, rating, finished, opinion):
+        self.cur.execute("INSERT INTO mybooks(title,author,rating,finished,opinion) VALUES(%s,%s,%s,%s,%s)",
+                    (title, author, rating, finished, opinion))
+        self.conn.commit()
 
-def connection():
-    global conn
-    conn = psycopg2.connect("dbname='books' user='postgres' password='admin' host='localhost' port='5432'")
-    # print("Data base connected successfully")
+    def view(self):
+        self.cur.execute("SELECT * FROM mybooks")
+        rows = self.cur.fetchall()
+        return rows
 
-def create_table():
-    connection()
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS mybooks(id SERIAL PRIMARY KEY NOT NULL,title VARCHAR(100) NOT NULL, author VARCHAR(100) NOT NULL,rating FLOAT,finished DATE,opinion TEXT)")
-    conn.commit()
-    conn.close()
+    def delete(self,id):
+        self.cur.execute("DELETE FROM mybooks WHERE id=%s", (id,))
+        self.conn.commit()
 
-def add_book(title,author,rating,finished,opinion):
-    connection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO mybooks(title,author,rating,finished,opinion) VALUES(%s,%s,%s,%s,%s)",(title,author,rating,finished,opinion))
-    conn.commit()
-    conn.close()
+    def update(self,id, title, author, rating, finished, opinion):
+        self.cur.execute("UPDATE mybooks SET title=%s, author=%s, rating=%s, finished=%s, opinion=%s WHERE id=%s ",
+                    (title, author, rating, finished, opinion, id))
+        self.conn.commit()
 
+    def search(self,title="", author=""):
+        self.cur.execute("SELECT * FROM mybooks WHERE title=%s OR author=%s",
+                    (title, author))
+        rows = self.cur.fetchall()
+        return rows
 
-def view():
-    connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM mybooks")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-
-
-def delete(id):
-    connection()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM mybooks WHERE id=%s",(id,))
-    conn.commit()
-    conn.close()
-
-def update(id,title,author,rating,finished,opinion):
-    connection()
-    cur = conn.cursor()
-    cur.execute("UPDATE mybooks SET title=%s, author=%s, rating=%s, finished=%s, opinion=%s WHERE id=%s ",(title,author,rating,finished,opinion,id))
-    conn.commit()
-    conn.close()
-
-
-
-def search(title="",author=""):
-    connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM mybooks WHERE title=%s OR author=%s",(title,author))
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-    
-
+    def __del__(self):
+        self.conn.close()
